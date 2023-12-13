5967389a0baaa37ddbfe220e234a4251c376c69e
Eduardo Oliveira <edu.oliveira1689@gmail.com>
2023-12-13 19:57:13 -0300

    criação da pagina inicial do App

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { SafeAreaView, Text } from "react-native"
+
+
+const App = () => {
+
+  return (
+    <SafeAreaView>
+      <Text> Olá Mundo </Text>
+      <Text> Olá Mundo </Text>
+      <Text> Olá Mundo </Text>
+      <Text> Olá Mundo </Text>
+      <Text> Olá Mundo </Text>
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
